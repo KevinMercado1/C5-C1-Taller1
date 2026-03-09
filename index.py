970a53a7120313047652cd1a8b2164bf87547b8b
Kevin <kevinmercado1912000@icloud.com>
2026-03-09 09:38:17 -0500

    python-K

---
+++ index.py
@@ -0,0 +1,13 @@
+#Listas
+
+colors = ['amarillo', 'naranja', 'verde']
+
+print(colors)
+colors.append('rojo')
+colors.append('azul')
+colors.append('azul')
+colors.append(25)
+colors.append(True)
+print(colors)
+colors.clear()
+print(colors)
